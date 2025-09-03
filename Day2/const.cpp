257505f67410c8053d3d8c9df4d9ba5d74632716
satyamshahi31 <satyamkumarshahi31@gmail.com>
2025-09-03 18:06:48 +0530

    Day2

---
+++ Day2/const.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    const int a = 5; // constant variable
+    a = 10; //cannot asssign another variable
+
+    
+    
+    cout<<a;
+
+    return 0;
+}
