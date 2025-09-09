c73fb6bc3b8b8020d6bfe56784c2091ce0d61645
satyamshahi31 <satyamkumarshahi31@gmail.com>
2025-09-09 11:32:25 +0530

    While_Loops

---
+++ Day5/int_max.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<climits> // for maximum and minimum
+using namespace std;
+int main()
+{
+    // int a = INT_MAX; //maximum value an interger variable can hold
+    // a++;
+    // cout<<a<<endl;
+    int b = INT_MIN; //minimum value an integer variable can hold
+    b--;
+    cout<<b<<endl;
+    b--;
+    cout<<b<<endl;
+    return 0;
+}
---
+++ Day5/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+int b = INT_MIN; //minimum value an integer variable can hold
+
+    // cout<<b<<endl;
